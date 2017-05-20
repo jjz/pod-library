861d7b54fc704bd58b4ca84fea9ae6126fa3ff88
jjz <woaf1003@gmail.com>
2017-05-20 13:20:22 +0800

    add PodLibrary

---
+++ pod-library/Classes/PodLibrary.swift
@@ -0,0 +1,15 @@
+//
+//  Podlibrary.swift
+//  Pods
+//
+//  Created by jjz on 20/05/2017.
+//
+//
+
+import Foundation
+
+class PodLibrary: NSObject {
+    func testPodLibrary(){
+        print("log from pod library")
+    }
+}
